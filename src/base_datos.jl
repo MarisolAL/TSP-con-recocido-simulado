6865a7699e57d042888786ae068c99fa4738b706
Unknown <marisolamezcua@ciencias.unam.mx>
2018-09-13 22:16:31 -0500

    Eliminadas caracteristicas innecesarias

--- src/base_datos.jl
+++ src/base_datos.jl
@@ -7,11 +7,6 @@ using StatsBase
 base_datos = SQLite.DB("ciudades.db")
 ciudadesdb = SQLite.query(base_datos, "SELECT * FROM cities")
 
-#Parte de la configuracion
-ciudades_del_problema = [1,2,3,28,74,163,164,165,166,167,169,326,327,328,329,330,489,490,491,492,493,494,495,653,654,655,658,666,814,815,816,817,818,819,978,979,980,981,1037,1073]
-
-no_total_ciudades = first(convert(Array,SQLite.query(base_datos, "SELECT COUNT(*) FROM cities")))
-no_ciudades  = size(ciudades_del_problema)[1]
 
 #Funcion que regresa una tabla con las ciudades que utilizaremos
 #esta es una subtabla de la tabla original de connections
