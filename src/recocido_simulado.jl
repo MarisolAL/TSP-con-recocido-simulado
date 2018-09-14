faffa701f33af7b7f6e2c87aae47ee828826468d
Unknown <marisolamezcua@ciencias.unam.mx>
2018-09-13 23:33:37 -0500

    Documentacion terminada y corregido error
    Corregido error de cantidad de iteraciones y modificacion de la ruta original tanto dentro del algoritmo como al calcular la temperatura inicial.

--- src/recocido_simulado.jl
+++ src/recocido_simulado.jl
@@ -1,14 +1,15 @@
 include("base_datos.jl")
 using Base_Datos
+using StatsBase
 
 #Parte de la CONFIGURACION
 ciudades_del_problema = [1,2,3,28,74,163,164,165,166,167,169,326,327,328,329,330,489,490,491,492,493,494,495,653,654,655,658,666,814,815,816,817,818,819,978,979,980,981,1037,1073]
-T_0 = 50
+T_0 = 100
 L = 500
 iter_max = 5000
 epsilon = 0.1
 phi = 0.5
-N = 500
+veces = 5000
 epsilon_p = 0.1
 P = 0.9
 #############################
@@ -16,7 +17,10 @@ N = Base_Datos.normalizador(ciudades_del_problema)
 grafica = Base_Datos.crea_matriz_adyacencias(ciudades_del_problema)
 
 
-"Funcion que calcula el costo de una ruta"
+"Funcion que calcula el costo de una ruta
+#Arguments
+- ruta:: Array{Int64,1}: Ruta a la que le obtendremos el costo
+- norm:: Float64: Normalizador del problema"
 function costo(ruta,norm)
     length = size(ruta)[1]
     suma = 0
@@ -71,18 +75,22 @@ function costo_permutacion(costo_actual, ruta, v_1, v_2,norm)
     if indice_v_2 == 1 #Un indice esta al inicio y el otro no esta al final
         suma = suma - grafica[v_2,ruta[indice_v_2+1]] +  grafica[v_1,ruta[indice_v_2+1]]
         suma = suma - grafica[v_1,ruta[indice_v_1+1]] - grafica[v_1,ruta[indice_v_1-1]]
-        suma = suma + grafica[v_2,ruta[indice_v_1+1]] + grafica[v_2,ruta[indice_v_1-1]]
+        suma = suma + grafica[v_2,ruta[indice_v_1+1]] + grafica[v_2,ruta[indice_v_1-1]]#REVISAAAR, no funciona porque se suman consigo mismos, dado que no cambio de posicion, si son contiguos se suman con su vecino que son ellos mismos
         return suma/norm
     end
     suma = suma - grafica[v_1,ruta[indice_v_1+1]] - grafica[v_1,ruta[indice_v_1-1]]
     suma = suma - grafica[v_2,ruta[indice_v_2+1]] - grafica[v_2,ruta[indice_v_2-1]]
     suma = suma + grafica[v_1,ruta[indice_v_2+1]] + grafica[v_1,ruta[indice_v_2-1]]
-    suma = suma + grafica[v_2,ruta[indice_v_1+1]] + grafica[v_2,ruta[indice_v_1-1]]
+    suma = suma + grafica[v_2,ruta[indice_v_1+1]] + grafica[v_2,ruta[indice_v_1-1]]#REVISAAAR
     suma = suma/norm
     return suma
 end
 
-"Funcion que permuta dos ciudades en una ruta, modifica la ruta original"
+"Funcion que permuta dos ciudades en una ruta, modifica la ruta original
+#Arguments
+- ruta:: Array{Int64,1}: Ruta original para permutar
+- v_1:: Int64: Primer vertice a permutar
+- v_2:: Int64: Segundo vertice a permutar"
 function permuta(ruta, v_1, v_2)
     indice_v_1 = Base_Datos.find_id(ruta,v_1)
     indice_v_2 = Base_Datos.find_id(ruta,v_2)
@@ -93,13 +101,19 @@ end
 
 "Funcion que obtiene una permutacion aleatoria usando la grafica
 para esto usamos los id's en graica para luego despues hacer la correspondencia con
-los verdaderos id's localizados en la base de datos "
+los verdaderos id's localizados en la base de datos
+#Arguments
+- longitud:: Int64: Longitud de la ruta"
 function obten_permutacion_aleatoria(longitud)
     ruta = sample(1:longitud, longitud, replace=false)#Obtenemos dos indices aleatorios
     return ruta
 end
 
-"Funcion que calcula el lote (soluciones aceptadas)"
+"Funcion que calcula el lote (soluciones aceptadas)
+#Arguments
+- T:: Float64: Temperatura inicial
+- S:: Ruta inicial
+- normalIzador:: Normalizador del sistema"
 function calcula_lote(T, S,normalIzador)
     c = 0
     i = 0
@@ -110,21 +124,24 @@ function calcula_lote(T, S,normalIzador)
         id_s = sample(1:size(ciudades_del_problema)[1], 2, replace=false) #obtenemos 2 id's
         id_1 = id_s[1]
         id_2 = id_s[2]
-        s_1 = permuta(S,id_1,id_2)
+        s_1 = permuta(copy(S),id_1,id_2)
         costo_aux = costo(s_1, normalIzador)
         if costo_aux < costo_i + T
             S = permuta(S,id_1,id_2)
             c = c+1
             r = r + costo_aux
             costo_i = costo_aux #Para no recalcular
-            println(string(">", costo_i))
         end
         i += 1
     end
     return [r/L,S]
 end
 
-"Funcion que se encarga del recocido simulado"
+"Funcion que se encarga del recocido simulado
+#Arguments
+- T:: Float64: Temperatura inicial
+- S:: Ruta inicial
+- normalIzador:: Normalizador del sistema"
 function aceptacion_por_umbrales(T,S,normalIzador)
     p = 0
     while T > epsilon
@@ -140,26 +157,35 @@ function aceptacion_por_umbrales(T,S,normalIzador)
     return S
 end
 
-"Funcion que calcula el porcentaje de soluciones aceptadas"
+"Funcion que calcula el porcentaje de soluciones aceptadas
+#Arguments
+- T:: Float64: Temperatura inicial
+- S:: Ruta actual
+- normalIzador:: Normalizador del sistema"
 function porcentaje_aceptados(S,T,normalIzador)
     c = 0
     costo_i = costo(S,normalIzador)
-    for i in 1:N
+    for i in 1:veces
         #Obtenemos una permutacion
         id_s = sample(1:size(ciudades_del_problema)[1], 2, replace=false) #obtenemos 2 id's
         id_1 = id_s[1]
         id_2 = id_s[2]
-        s_1 = permuta(S,id_1,id_2)
+        s_1 = permuta(copy(S),id_1,id_2)
         costo_aux = costo(s_1, normalIzador)
         if costo_aux < costo_i + T
             c = c+1
             S = permuta(S,id_1,id_2)
         end
     end
-    return c/N
+    return c/veces
 end
 
-"Funcion que realiza la busqueda binaria"
+"Funcion que realiza la busqueda binaria
+#Arguments
+- T_1:: Float64: Temperatura inferior de la busqueda
+- T_2:: Float64: Limite superior de la busqueda
+- s:: Ruta actual
+- normalIzador:: Normalizador del sistema"
 function busqueda_binaria(s, T_1, T_2, P,normalIzador)
     T_m = (T_1 + T_2) / 2
     if T_2 - T_1 < epsilon
@@ -176,7 +202,12 @@ function busqueda_binaria(s, T_1, T_2, P,normalIzador)
     end
 end
 
-"Funcion que calcula la temperatura inicial"
+"Funcion que calcula la temperatura inicial
+#Arguments
+- s:: Ruta inicial
+- T:: Float64: Temperatura inicial de la configuracion
+- P:: Float64: Probabilidad de aceptacion
+- normalIzador:: Normalizador del sistema"
 function  temperatura_inicial(s, T, P,normalIzador)
     p = porcentaje_aceptados(s, T,normalIzador)
     if abs(P - p) <= epsilon_p
@@ -200,12 +231,18 @@ function  temperatura_inicial(s, T, P,normalIzador)
     return busqueda_binaria(s, T_1, T_2, P,normalIzador)
 end
 
-"Funcion que nos dice si una solucion es factible"
+"Funcion que nos dice si una solucion es factible
+#Arguments
+- costo:: Float64: Costo de la ruta obtenida
+- normalIzador:: Float64: Normalizador del sistema"
 function es_factible(costo, normalIzador)
     return costo < 1
 end
 
-"Funcion que toma los id's en la matriz de adyacencias y regresa los de las ciudades"
+"Funcion que toma los id's en la matriz de adyacencias y regresa los de las ciudades
+#Arguments
+- ciudades_del_problema:: Array{Int64,1}: Lista con las ciudades que participan en el TSP
+- solucion:: Array{Int64,1}: Ruta obtenida por el algoritmo"
 function pasa_a_ids_reales(ciudades_del_problema, solucion)
     length = size(solucion)[1]
     respuesta = ones(length)
@@ -216,3 +253,22 @@ function pasa_a_ids_reales(ciudades_del_problema, solucion)
     end
     return respuesta
 end
+
+"Funcion que corre todo el algoritmo"
+function haz_todo(normalIzador)
+    #Tomando instancia
+    length = size(ciudades_del_problema)[1]
+    s_0 = obten_permutacion_aleatoria(length)#permutacion aleatoria
+    T = temperatura_inicial(copy(s_0),T_0,P,normalIzador)
+    solucion = aceptacion_por_umbrales(T,s_0,normalIzador)
+    solucion2 = pasa_a_ids_reales(ciudades_del_problema,solucion)
+    costo_f = costo(solucion, normalIzador)
+    return[solucion2,costo_f,es_factible(costo_f,normalIzador)]
+end
+
+resp = haz_todo(N)
+minimo = resp[2]
+s = resp[1]
+println(minimo)
+println(s)
+println(resp[3])
